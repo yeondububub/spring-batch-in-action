1936236990451f07e7e102f25bab827b0ea3958d
ydbb <yeondububub@gmail.com>
2026-08-02 13:32:04 +0900

    feat: ItemProcessor 구현

--- src/main/java/com/example/springbatchinaction/job/SettlementJobConfig.java
+++ src/main/java/com/example/springbatchinaction/job/SettlementJobConfig.java
@@ -1,11 +1,13 @@
 package com.example.springbatchinaction.job;
 
 import com.example.springbatchinaction.domain.Orders;
+import com.example.springbatchinaction.domain.Settlement;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.infrastructure.item.ItemProcessor;
 import org.springframework.batch.infrastructure.item.database.JpaPagingItemReader;
 import org.springframework.batch.infrastructure.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,5 +39,17 @@ public class SettlementJobConfig {
                 .queryString("SELECT o FROM ORDERS o WHERE o.orderDate = :targetDate ORDER BY o.id")
                 .parameterValues(Collections.singletonMap("targetDate", LocalDate.parse(targetDate)))
                 .build();
+    }
+
+    @Bean
+    public ItemProcessor<Orders, Settlement> settlementProcessor() {
+        log.info("[Processor] 정산 금액 계산");
+
+        return item -> {
+            int fee = (int) (item.getAmount() * 0.03);
+            int settlementAmount = item.getAmount() - fee;
+
+            return new Settlement(item.getId(), item.getStoreName(), settlementAmount, LocalDate.now());
+        };
     }
 }
