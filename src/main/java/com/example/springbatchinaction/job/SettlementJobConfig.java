174fc65d3c78ded5302fbfe0d79bcf8662b15bc5
ydbb <yeondububub@gmail.com>
2026-08-02 14:39:04 +0900

    feat: Step, Job 등록

--- src/main/java/com/example/springbatchinaction/job/SettlementJobConfig.java
+++ src/main/java/com/example/springbatchinaction/job/SettlementJobConfig.java
@@ -6,7 +6,11 @@ import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.infrastructure.item.ItemProcessor;
 import org.springframework.batch.infrastructure.item.database.JpaItemWriter;
 import org.springframework.batch.infrastructure.item.database.JpaPagingItemReader;
@@ -23,7 +27,6 @@ import java.util.Collections;
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-@StepScope
 public class SettlementJobConfig {
 
     private final JobRepository jobRepository;
@@ -31,14 +34,15 @@ public class SettlementJobConfig {
     private final EntityManagerFactory entityManagerFactory;
 
     @Bean
-    public JpaPagingItemReader<Orders> ordersReader(@Value("#{jobparameters['targetDate']}") String targetDate) {
+    @StepScope
+    public JpaPagingItemReader<Orders> ordersReader(@Value("#{jobParameters['targetDate']}") String targetDate) {
         log.info("[Reader] 정산 집계 대상 날짜: {}", targetDate);
 
         return new JpaPagingItemReaderBuilder<Orders>()
                 .name("ordersReader")
                 .entityManagerFactory(entityManagerFactory)
                 .pageSize(1000)
-                .queryString("SELECT o FROM ORDERS o WHERE o.orderDate = :targetDate ORDER BY o.id")
+                .queryString("SELECT o FROM Orders o WHERE o.orderDate = :targetDate ORDER BY o.id")
                 .parameterValues(Collections.singletonMap("targetDate", LocalDate.parse(targetDate)))
                 .build();
     }
@@ -56,11 +60,31 @@ public class SettlementJobConfig {
     }
 
     @Bean
-    public JpaItemWriter<Settlement> settlementJpaItemWriter() {
+    public JpaItemWriter<Settlement> settlementWriter() {
         log.info("[Writer] 등록 처리");
 
         return new JpaItemWriterBuilder<Settlement>()
                 .entityManagerFactory(entityManagerFactory)
+                .build();
+    }
+
+    // Job 등록
+    @Bean
+    public Job settlementJob(Step settlementStep) {
+        return new JobBuilder("settlementJob", jobRepository)
+                .start(settlementStep)
+                .build();
+    }
+
+    // Step 등록
+    @Bean
+    public Step settlementStep(JpaPagingItemReader<Orders> ordersReader) {
+        return new StepBuilder("settlementStep", jobRepository)
+                .<Orders, Settlement>chunk(1000)
+                .transactionManager(transactionManager)
+                .reader(ordersReader)
+                .processor(settlementProcessor())
+                .writer(settlementWriter())
                 .build();
     }
 }
